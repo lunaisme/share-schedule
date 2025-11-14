ff305c8b41b3dbe9fc6a5f3ddebb8ec14dc7dce4
lunaisme <habibmaulana25.2003@gmail.com>
2025-11-14 23:30:07 +0700

    fix bug

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -28,9 +28,9 @@ export default function LoginPage() {
       const { error } = await supabase.auth.signInWithPassword({
         email,
         password,
-        options: {
-          emailRedirectTo: process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || `${window.location.origin}/dashboard`,
-        },
+        // options: {
+        //   emailRedirectTo: process.env.NEXT_PUBLIC_DEV_SUPABASE_REDIRECT_URL || `${window.location.origin}/dashboard`,
+        // },
       })
       if (error) throw error
       router.push("/dashboard")
